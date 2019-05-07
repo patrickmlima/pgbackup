0924947fe1d45d4f54b628d7445ea5fc2cc23101
Patrick Lima <patrickm.lima1@gmail.com>
2019-05-07 19:39:40 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+setup(
+	name='pgbackup',
+	version='0.1.0',
+	author='Patrick Lima',
+	author_email='patrickm.lima1@gmail.com',
+	description='A utility for backing up PostgreSQL databases.',
+	long_description_content_type='text/markdown',
+	url='https://github.com/patrickmlima/pgbackup',
+	packages=find_packages('src')
+)
+
